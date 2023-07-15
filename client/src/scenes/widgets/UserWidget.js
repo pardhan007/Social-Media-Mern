6b052a157dbb15ebf032e1b0c3ca6a5e2e014031
Harsh Prajapati <golaharsh2001@gmail.com>
2023-07-16 00:45:36 +0530

    now user can add thier twitter and linkedin(temp)

--- client/src/scenes/widgets/UserWidget.js
+++ client/src/scenes/widgets/UserWidget.js
@@ -1,10 +1,19 @@
 import {
+    Check,
     EditOutlined,
     LocationOnOutlined,
     ManageAccountsOutlined,
     WorkOutlineOutlined,
 } from "@mui/icons-material";
-import { Box, Divider, Typography, useTheme } from "@mui/material";
+import {
+    Box,
+    Button,
+    Divider,
+    IconButton,
+    Input,
+    Typography,
+    useTheme,
+} from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import UserImage from "components/UserImage";
 import WidgetWrapper from "components/WidgetWrapper.js";
@@ -18,11 +27,18 @@ const UserWidget = ({ userId, picturePath }) => {
     const { palette } = useTheme();
     const navigate = useNavigate();
     const token = useSelector((state) => state.token);
+    const loggedInUserId = useSelector((state) => state.user._id);
     const server = useSelector((state) => state.server);
     // const friends = useSelector((state) => state.user.friends);
     const dark = palette.neutral.dark;
     const medium = palette.neutral.medium;
     const main = palette.neutral.main;
+
+    const [twitterOpen, setTwitterOpen] = useState(false);
+    const [linkedinOpen, setLinkedinOpen] = useState(false);
+
+    const [twitterUrl, setTwitterUrl] = useState("");
+    const [linkedinUrl, setLinkedinUrl] = useState("");
 
     const getUser = async () => {
         try {
@@ -47,6 +63,7 @@ const UserWidget = ({ userId, picturePath }) => {
     }
 
     const {
+        _id,
         firstName,
         lastName,
         occupation,
@@ -145,34 +162,102 @@ const UserWidget = ({ userId, picturePath }) => {
                 >
                     Social Profiles
                 </Typography>
-                <FlexBetween gap="1rem" mb="0.5rem">
+                <FlexBetween gap="1rem" mb="0.5rem" paddingY="0.2rem">
                     <FlexBetween gap="1rem">
                         <img src="../assets/twitter.png" alt="twitter" />
                         <Box>
-                            <Typography color={dark} fontWeight="500">
-                                Twitter
-                            </Typography>
-                            <Typography color={medium}>
-                                Social Network
-                            </Typography>
+                            {twitterOpen ? (
+                                <Input
+                                    placeholder="Enter Twitter Username"
+                                    value={twitterUrl}
+                                    onChange={(e) =>
+                                        setTwitterUrl(e.target.value)
+                                    }
+                                ></Input>
+                            ) : (
+                                <Button
+                                    size="small"
+                                    color="primary"
+                                    variant="contained"
+                                >
+                                    {twitterUrl ? (
+                                        <a
+                                            href={`https://twitter.com/${twitterUrl}`}
+                                            target="_blank"
+                                            rel="noreferrer"
+                                            style={{ textDecoration: "none" }}
+                                        >
+                                            Twitter
+                                        </a>
+                                    ) : (
+                                        <Typography variant="">
+                                            Twitter
+                                        </Typography>
+                                    )}
+                                </Button>
+                            )}
                         </Box>
                     </FlexBetween>
-                    <EditOutlined color={dark} />
+                    {loggedInUserId === _id && (
+                        <IconButton
+                            onClick={() => setTwitterOpen(!twitterOpen)}
+                        >
+                            {twitterOpen ? (
+                                <Check color="primary" />
+                            ) : (
+                                <EditOutlined color={dark} />
+                            )}
+                        </IconButton>
+                    )}
                 </FlexBetween>
 
                 <FlexBetween gap="1rem">
-                    <FlexBetween gap="1rem">
+                    <FlexBetween gap="1rem" paddingY="0.2rem">
                         <img src="../assets/linkedin.png" alt="linkedin" />
                         <Box>
-                            <Typography color={dark} fontWeight="500">
-                                Linkedin
-                            </Typography>
-                            <Typography color={medium}>
-                                Network Platform
-                            </Typography>
+                            {linkedinOpen ? (
+                                <Input
+                                    placeholder="Enter Linkedin Username"
+                                    value={linkedinUrl}
+                                    onChange={(e) =>
+                                        setLinkedinUrl(e.target.value)
+                                    }
+                                ></Input>
+                            ) : (
+                                <Button
+                                    size="small"
+                                    color="primary"
+                                    variant="contained"
+                                >
+                                    {linkedinUrl ? (
+                                        <a
+                                            href={`https://www.linkedin.com/in/${linkedinUrl}`}
+                                            target="_blank"
+                                            rel="noreferrer"
+                                            style={{ textDecoration: "none" }}
+                                        >
+                                            LinkedIn
+                                        </a>
+                                    ) : (
+                                        <Typography variant="">
+                                            LinkedIn
+                                        </Typography>
+                                    )}
+                                </Button>
+                            )}
                         </Box>
                     </FlexBetween>
-                    <EditOutlined color={dark} />
+                    {loggedInUserId === _id && (
+                        <IconButton
+                            onClick={() => setLinkedinOpen(!linkedinOpen)}
+                        >
+                            {linkedinOpen ? (
+                                <Check color="primary" />
+                            ) : (
+                                <EditOutlined color={dark} />
+                            )}
+                        </IconButton>
+                    )}
                 </FlexBetween>
             </Box>
         </WidgetWrapper>
